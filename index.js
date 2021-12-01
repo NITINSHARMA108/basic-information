69b6a8c91783e6f52f44c218bbd0f28cf4e129a5
nitin sharma <nitin.sharma@jalantechnologies.com>
2021-12-01 08:02:38 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // creating a local server
 const http=require('http');
 const fs =require('fs');
-const lookup=require('mime-types').lookup;
+
 
 const server = http.createServer((req,res)=>{
     console.log(req.url);
@@ -9,35 +9,55 @@ const server = http.createServer((req,res)=>{
     
     switch(req.url){
         case '/':fs.readFile('./views/index.html',(err,data)=>{
-                res.write(data);
-                // res.end();
+                    if(err)
+                    {
+                        res.end('Error Page 404');
+                        
+                    }
+                    res.end(data);
                     })
                     
                 
                 break;
         case '/about':fs.readFile('./views/about.html',(err,data)=>{
-                    res.write(data);
-                    // res.end();
+                    if(err)
+                    {
+                        res.end('Error Page 404');
+                    }
+                    
+                       res.end(data);
                     })
                     break;
         case '/contact':fs.readFile('./views/contact-me.html',(err,data)=>{
-                    res.write(data);
-                   // res.end();
-                })
-                break;
-        
-            default:fs.readFile('./views/404.html',(err,data)=>{
-                    res.write(data);
-                    res.end();
-
-                })
+                        if(err)
+                        {
+                            res.end('Error Page 404');
+                        }
+                        
+                        res.end(data);
+                        })
+                        break;
+        case '/styles.css': res.writeHead(200,{'Content-Type':'text/css'})
+                            fs.readFile('./views/styles.css',(err,data)=>{
+                                res.end(data);
+                            })
+                            break;
+                            /*res.write(fs.readFileSync('./views/styles.css'));
+                           // res.end();
+                           break; */
+        default:        fs.readFile('./views/404.html',(err,data)=>{
+                        // res.write(data);
+                        res.end(data);
+                        })
+                        break;
     }
+    
    
 
 
 
 })
 
-server.listen(8080,'localhost',()=>{
+server.listen(8080,()=>{
     console.log('listing for request on port 8080');
 })
